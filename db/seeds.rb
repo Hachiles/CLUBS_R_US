4f55ab6cb41bc989bd0b290612c1f24ac7b8a08c
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-17 12:13:32 +0100

    Seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+  20.times do
+    club = Club.new(
+        name: Faker::Game.title,
+        capacity: rand(50..2000),
+        hourly_price: rand(50..200),
+        address: Faker::Address.full_address,
+        description: Faker::Lorem.paragraph_by_chars
+        # image: Faker::LoremFlickr.image(size: '50x60', search_terms: ['clubs', 'venue']),
+    )
+    user = User.where(username: "Hachiles").first
+    club.user = user
+    club.save
+  end
+  
+puts 'Done!'
