5fd77a52c808e55563c9fc6cf2ba89287ac24b7e
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-19 16:20:42 +0100

    myBookings and myClubs now work perfectly, dashboard path deleted and refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/dashboard' => 'users#show', as: :dashboard
+  get '/mybookings' => 'bookings#show', as: :mybookings
+  get '/myclubs' => 'users#show', as: :myclubs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :clubs do
     resources :bookings, only: [ :index, :new, :create ]
