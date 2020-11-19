493c5d436ba2a40f2142f8245ac84cbbd8be73f4
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-19 17:21:43 +0100

    Map info cards done on index

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -6,7 +6,8 @@ class ClubsController < ApplicationController
     @markers = @clubs.geocoded.map do |club|
       {
         lat: club.latitude,
-        lng: club.longitude
+        lng: club.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { club: club })
       }
     end
   end
