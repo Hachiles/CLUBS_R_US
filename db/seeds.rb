2d484188ab48c3d1db6ac398449e3c4942b99b94
Ulysse Bondy <ulysse.bondy@hotmail.com>
2020-11-19 14:52:26 +0100

    Booking for mon show page, seed polished, and index pictures true

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
 require "open-uri"
-
-# seed_user = User.create(username: "seeder", email: "seeder@gmail.com", first_name: "seeder", last_name: "Mcseeder", password: "seed")
 
 real_addresses = [
   "Rudi-Dutschke-Straße 26, Berlin",
@@ -24,30 +17,38 @@ real_addresses = [
   "Brunnenstraße 198, Berlin"
 ]
 
-
 index_num = 0
 
+puts "Deleting current bookings (#{Booking.count} bookings to delete)"
+Booking.delete_all
+puts "All bookings deleted"
+puts " "
 
-puts "Starting seed"
+puts "Deleting current clubs (#{Club.count} clubs to delete)"
+Club.delete_all
+puts "All clubs deleted"
+puts " "
 
+puts "Beginning the seeding process"
+puts "Seeding, please wait..."
 
-  6.times do
+  10.times do
     file = URI.open('https://source.unsplash.com/1600x900/?club')
 
-    index_num = index_num += 1 
-    # puts "Generating faker seed!" 
-    # puts index_num
     club = Club.new(
-        name: Faker::Game.title,
-        capacity: rand(50..2000),
-        hourly_price: rand(50..200), 
-        address: real_addresses[index_num],
-        description: Faker::Lorem.paragraph_by_chars,
-    )
+      name: Faker::Game.title,
+      capacity: rand(50..2000),
+      hourly_price: rand(50..200), 
+      address: real_addresses[index_num],
+      description: Faker::Lorem.paragraph_by_chars,
+      )
+
     club.photo.attach(io: file, filename: 'club.png', content_type: 'image/png')
-    # had to modify this for heroku push
     club.user = User.first
     club.save
+    index_num = index_num += 1 
   end
-  
-puts 'Done!'
+
+puts " "
+puts "Seeding process completed!"
+puts "The user account of #{User.first.username} was used to generate #{Club.count} Clubs"
